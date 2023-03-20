1ee8c744b719ca86f00dbc801929ed372512d8b5
MagdalenaKuswik <122886488+MagdalenaKuswik@users.noreply.github.com>
2023-03-20 17:36:21 +0100

    Task 3: principal component analysis-PCA

---
+++ task_3.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("cleaned_data.csv", sep = "," , encoding= 'utf-8')
+frame = df.drop(['Credit_Score'], axis = 1)
+creditScore = df['Credit_Score']
+
+print(frame.head())
+
+print(frame.shape[1])
+
+print(frame.info())
+
+pca = PCA(svd_solver= 'full', n_components= 0.80)
+glowne = pca.fit_transform(frame)
+glowneFrame = pd.DataFrame(data=glowne)
+
+print(glowneFrame.head())
+
+print(glowneFrame.shape[1])
+
+print(glowneFrame.info())
+
+x_train, x_test, y_train, y_test = train_test_split(glowneFrame, creditScore, test_size= 0.4, random_state=75)
+
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
